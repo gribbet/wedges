e0789526adbd55284fcae0b38134a9295ec75102
Graham <graham.gibbons@gmail.com>
2018-01-24 22:34:16 -0800

    Add loading callback

--- src/Application.ts
+++ src/Application.ts
@@ -1,11 +1,16 @@
 import Component from "./Component";
 
 export default class Application {
+    private loading: (loading: boolean) => void =
+        loading => undefined;
 
     constructor(
         private component: Component,
-        private element: Element
-    ) { }
+        private element: Element,
+        loading?: (loading: boolean) => void
+    ) {
+        this.loading = loading || this.loading;
+    }
 
     private readonly started = new Promise(resolve =>
         this.start = async () => {
@@ -20,11 +25,12 @@ export default class Application {
 
     update(trigger?: () => void) {
         return this.last = this.last.then(async () => {
-            console.log("Update");
+            this.loading(true);
             if (trigger) trigger();
             await this.component.load();
             this.start();
             (await this.rendering).update();
+            this.loading(false);
         })
     }
 
